b5cb34b55cdbe2b1a78473d7b96918a6a848806a
Nishchit Bhandari <nishchitbhandari2061@gmail.com>
2025-01-29 12:40:05 +0545

    Initial commit: basic sheets operations

---
+++ sheet/config.py
@@ -0,0 +1,22 @@
+import gspread
+from google.oauth2.service_account import Credentials
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+# Scope
+scopes = [
+    "https://www.googleapis.com/auth/spreadsheets"
+]
+
+#
+creds = Credentials.from_service_account_file(
+    "credentials.json", scopes=scopes)
+
+client = gspread.authorize(creds)
+sheets_id = os.getenv("SHEET_ID")
+sheet = client.open_by_key(sheets_id)
+worksheet = sheet.sheet1
+
---
+++ sheet/sheet_operations.py
@@ -0,0 +1,35 @@
+from .config import worksheet
+import pandas as pd
+
+df = pd.DataFrame(worksheet.get_all_records())
+
+
+def get_top_k_individual(k: int):
+    sorted_data = df.sort_values(by="Score").head(k)
+    # Converting Pandas DF to list of dict so that it becomes easier for sending API Response
+    list_of_dicts = sorted_data.to_dict(orient='records')
+    return list_of_dicts
+
+
+def get_top_k_place(place: str, k: int):
+    place = place.capitalize()
+    top_k_places = df.groupby(place)["Score"].mean().sort_values(
+        ascending=False).head(k).reset_index(name='Average Score')
+    list_of_dicts = top_k_places.to_dict(orient="records")
+    return list_of_dicts
+
+
+def add_new_user(name, username, province, district, school):
+    new_user = [name, username, province, district, school, 0]
+    worksheet.append_row(new_user)
+
+
+def increase_score(username, increase_point):
+    cell = worksheet.find(username)
+    row_number = cell.row
+    current_score = int(worksheet.cell(row_number, 6).value)
+    new_score = current_score + increase_point
+    worksheet.update_cell(row_number, 6, new_score)
+    return (f"Updated {username}'s score to {new_score}")
+
+
